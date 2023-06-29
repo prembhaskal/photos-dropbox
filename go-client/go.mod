fffebd614d2fc92815b3e87288b5321f3ddc6e1e
bhaskal <premkumar.bhaskal@nokia.com>
2023-06-29 21:00:27 +0530

    added vendor, workflow to get token in background

--- go-client/go.mod
+++ go-client/go.mod
@@ -2,7 +2,11 @@ module github.com/prembhaskal/photos-dropbox/go-client
 
 go 1.20
 
-require golang.org/x/oauth2 v0.9.0
+require (
+	github.com/jeremywohl/flatten v1.0.1
+	github.com/kelseyhightower/envconfig v1.4.0
+	golang.org/x/oauth2 v0.9.0
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
--- go-client/main.go
+++ go-client/main.go
@@ -2,17 +2,85 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/jeremywohl/flatten"
+	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/oauth2"
 )
 
+type PhotosConfig struct {
+	TokenFile string `envconfig:"TOKEN_FILE" default:"/var/tmp/client_info.json"`
+}
+
 func main() {
+	clientID, clientSecret, err := getClientSecret()
+	if err != nil {
+		panic(fmt.Sprintf("unable to read clientID or secret, check env variable TOKEN_FILE, err: %v", err))
+	}
+
+	tokenChan := make(chan string, 0)
+	go getToken(clientID, clientSecret, tokenChan)
+
+	var token string
+	select {
+	case token = <-tokenChan:
+		break
+	case <-time.After(1 * time.Minute):
+		log.Printf("timed out obtaining token, exiting...")
+		panic("timed out token")
+	}
+
+	log.Printf("processing with token: %s", token)
+}
+
+func getClientSecret() (string, string, error) {
+	conf := &PhotosConfig{}
+	err := envconfig.Process("", conf)
+	if err != nil {
+		return "", "", err
+	}
+
+	// read the file
+	data, err := os.ReadFile(conf.TokenFile)
+	if err != nil {
+		return "", "", fmt.Errorf("error reading file: %s, err: %w", conf.TokenFile, err)
+	}
+
+	var jsonval map[string]any
+
+	err = json.Unmarshal(data, &jsonval)
+	if err != nil {
+		return "", "", err
+	}
+
+	nested, err := flatten.Flatten(jsonval, "", flatten.DotStyle)
+	if err != nil {
+		return "", "", err
+	}
+
+	clientID := nested["web.client_id"].(string)
+	clientSecret := nested["web.client_secret"].(string)
+	if len(clientID) == 0 {
+		return "", "", fmt.Errorf("empty clientID")
+	}
+	if len(clientSecret) == 0 {
+		return "", "", fmt.Errorf("empty client secret")
+	}
+
+	return clientID, clientSecret, nil
+
+}
+
+func getToken(clientID, clientSecret string, tokenChan chan<- string) {
 	conf := &oauth2.Config{
-		ClientID:     "", // TODO - get from env. / json file
-		ClientSecret: "", // TODO - get from env. / json file
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
 		Scopes: []string{
 			"https://www.googleapis.com/auth/drive.metadata.readonly",
 			"https://www.googleapis.com/auth/photoslibrary.readonly"},
@@ -26,14 +94,16 @@ func main() {
 	mux := &http.ServeMux{}
 	mux.HandleFunc("/", loadAuthPage)
 	mux.HandleFunc("/login-google", loginGoogle(conf))
-	mux.HandleFunc("/callback", callbackHandler(conf))
+	mux.HandleFunc("/callback", callbackHandler(conf, tokenChan))
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    "0.0.0.0:8080",
 	}
 
-	fmt.Printf("start server ... ")
+	log.Println("starting server ...")
+	log.Println("open localhost:8080 and follow steps to get token")
+
 	panic(server.ListenAndServe())
 }
 
@@ -41,7 +111,7 @@ func loadAuthPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./html/login.html")
 }
 
-func callbackHandler(conf *oauth2.Config) http.HandlerFunc {
+func callbackHandler(conf *oauth2.Config, tokenChan chan<- string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("got request: %+v in callback\n", r)
 
@@ -63,7 +133,7 @@ func callbackHandler(conf *oauth2.Config) http.HandlerFunc {
 		}
 
 		log.Printf("token obtained: %+v", tok)
-
+		tokenChan <- tok.AccessToken
 	}
 }
 
